c30042efa61f54746902426d69385c7a26eb107c
Jared Praino <jtpraino@uw.edu>
2016-02-21 15:41:13 -0800

    Added comments to chart_1

--- scripts/chart_1.R
+++ scripts/chart_1.R
@@ -6,16 +6,22 @@
 library(dplyr)
 library(plotly)
 
+# A function that returns a stacked bar chart representing which operating system
+# each student uses separate by class standing.
 chart_1 <- function(data) {
+  
+  # Selecting relevant data
   chart_data <- data %>% select(What.is.your.current.class.standing., 
                                 What.operating.system.do.you.typically.use.)
   
+  # Creating proper names for relevant data
   chart_data <- chart_data %>% rename("Class Standing"              = What.is.your.current.class.standing., 
                                       "Operating System"            = What.operating.system.do.you.typically.use.)
   
   levels(chart_data$`Operating System`)[levels(chart_data$`Operating System`)
                                         =="Other: Windows at home, Linux (Ubuntu) at work"] <- "Other"
   
+  # Formatting data
   df <- chart_data %>% group_by(`Class Standing`) %>% 
     summarise(
       windows = length(which(`Operating System`=="Windows")),
@@ -26,6 +32,7 @@ chart_1 <- function(data) {
     mutate(order = c(1,3,4,2)) %>% 
     arrange(order)
   
+  # Creating and displaying a stacked bar graph
    plot_ly(
     x = df$`Class Standing`,
     y = df$windows,
