ecf3535eb0f3c331903550d8c2963b62e0e20934
Kyle Wistrand <kyle.wistrand@gmail.com>
2016-02-21 16:23:55 -0800

    Added comments to code and commented out everything except function, for production. This resolves 4

--- scripts/chart_2.R
+++ scripts/chart_2.R
@@ -1,21 +1,25 @@
-library(plotly)
-library(dplyr)
-
-setwd("/Users/kylewistrand/Documents/Homework/INFO_201/a7-collaborative-coding/")
-
-data <- read.csv("data/intro_survey_data.csv")
-
-colnames(data) <- c("class", "major", "os", "command", "git", "markdown", "r", "programming", "countries", "animals", "seahawks")
-
-data <- mutate(data, "class_num" = NA)
-data$class_num[data$class == "Freshman"] <- 1
-data$class_num[data$class == "Sophomore"] <- 2
-data$class_num[data$class == "Junior"] <- 3
-data$class_num[data$class == "Senior"] <- 4
-
-data <- arrange(data, class_num)
+# For testing only:
+#library(plotly)
+#library(dplyr)
+#setwd("/Users/kylewistrand/Documents/Homework/INFO_201/a7-collaborative-coding/")
+#data <- read.csv("data/intro_survey_data.csv")
 
 chart_2 <- function(data) {
+  # Initially change the column names so they're easier to work with and more clear
+  colnames(data) <- c("class", "major", "os", "command", "git", "markdown",
+                      "r", "programming", "countries", "animals", "seahawks")
+  
+  # Make a new column with class standing as a number
+  data <- mutate(data, "class_num" = NA)
+  data$class_num[data$class == "Freshman"] <- 1
+  data$class_num[data$class == "Sophomore"] <- 2
+  data$class_num[data$class == "Junior"] <- 3
+  data$class_num[data$class == "Senior"] <- 4
+  
+  # Sort data by class, so plot is in order
+  data <- arrange(data, class_num)
+  
+  # Make and return a box and whisker plot
   plot_ly(data,
     x = class,
     y = countries,
@@ -28,4 +32,4 @@ chart_2 <- function(data) {
   )
 }
 
-chart_2(data)
+#chart_2(data)
